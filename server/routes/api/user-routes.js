349c443d7dba41a68190f46333d004fd19ed5fc9
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-07-21 23:26:51 -0400

    added starter code

---
+++ server/routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  createUser,
+  getSingleUser,
+  saveBook,
+  deleteBook,
+  login,
+} = require('../../controllers/user-controller');
+
+// import middleware
+const { Middleware } = require('../../utils/');
+
+// put Middleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(Middleware, saveBook);
+
+router.route('/login').post(login);
+
+router.route('/me').get(Middleware, getSingleUser);
+
+router.route('/books/:bookId').delete(Middleware, deleteBook);
+
+module.exports = router;
